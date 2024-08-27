12418b271f64d62fc32de0dc5ad97baedf718f66
dev-rohan2024 <dev-rohan2024@gmail.com>
2024-08-27 21:24:55 +0600

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+
   theme: {
-    extend: {},
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        "text-gray": "#494949",
+        "text-light": "#737373",
+        "header-btn-bg": "#ABEF5F",
+        "paragaph-text": "#494949",
+        "hero-btn": "rgb(171 239 95 / 67%)",
+      },
+    },
   },
   plugins: [],
 };
